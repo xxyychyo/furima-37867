923b0b8a68cb8f07ab4901197221b900ef28b76b
xxyychyo <103950402+xxyychyo@users.noreply.github.com>
2022-05-31 20:32:00 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update,]
 
 
   def index
@@ -23,14 +24,38 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    #binding.pry
+    if@item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      # NGであれば、エラー内容とデータを保持したままeditファイルを読み込み、エラーメッセージを表示させる
+      render 'edit'
+    end
+  end
+
+
 private
 
  def item_params
   params.require(:item).permit(:image, :name, :explanation, :category_id, :derively_fee_id, :status_id, :prefecture_id, :send_day_id, :price).merge(user_id: current_user.id)
  end
+
+ def move_to_index
+  @item = Item.find(params[:id])
+  unless current_user == @item.user #現在ログインしている人がプロトタイプ投稿者以外だったら
+    redirect_to action: :index
+  end
+ end
   # def set_item
   #  @item = Item.find(params[:id])
-  # end
+  # end  binding.pry
 
 
 end
